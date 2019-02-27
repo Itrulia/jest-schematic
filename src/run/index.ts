03b832263636746747ecf9652352b7965518005a
Karl Merkli <k.merkli@eslgaming.com>
2019-02-27 15:37:01 +0100

    fix: not throwing error on tests failing

--- src/run/index.ts
+++ src/run/index.ts
@@ -1,9 +1,8 @@
-import {BuilderConfiguration, BuildEvent, BuilderContext, Builder} from "@angular-devkit/architect";
-import {Observable, from as fromPromise} from "rxjs";
-import {mapTo} from "rxjs/operators";
-import {JestBuilderSchema} from "./schema";
-import {isArray} from "lodash";
-const {runCLI} = require("jest");
+import { BuilderConfiguration, BuildEvent, BuilderContext, Builder } from "@angular-devkit/architect";
+import { Observable, from as fromPromise } from "rxjs";
+import { JestBuilderSchema } from "./schema";
+import { isArray } from "lodash";
+const { runCLI } = require("jest");
 
 export default class JestBuilder implements Builder<JestBuilderSchema> {
     constructor(public context: BuilderContext) {}
@@ -20,23 +19,28 @@ export default class JestBuilder implements Builder<JestBuilderSchema> {
             return opt;
         }, {});
 
-        const shell = runCLI({
-            ...options,
-            rootDir: options.rootDir
-                ? options.rootDir
-                : this.context.workspace.root,
-            globals: options.globals
-                ? options.globals
-                : {
-                    "ts-jest": {
-                      tsConfigFile: options.tsConfig
-                    },
-                    "__TRANSFORM_HTML__": true
-                  },
-        }, [__dirname]);
+        const shell = runCLI(
+            {
+                ...options,
+                rootDir: options.rootDir ? options.rootDir : this.context.workspace.root,
+                globals: options.globals
+                    ? options.globals
+                    : {
+                          "ts-jest": {
+                              tsConfigFile: options.tsConfig,
+                          },
+                          __TRANSFORM_HTML__: true,
+                      },
+            },
+            [__dirname]
+        ).then(({ results }) => {
+            if (!options.watch && !options.watchAll && (results.numFailedTestSuites || results.numFailedTests)) {
+                return { success: false };
+            }
 
-        return fromPromise(shell).pipe(
-            mapTo({success: true}),
-        );
+            return { success: true };
+        });
+
+        return fromPromise(shell);
     }
 }
